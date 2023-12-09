d65a9e89bfc4fb5caff785bc08a4d49e302d8e15
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2023-12-09 11:13:06 +0530

    promotion rolePermission

--- rolePermission/permission.js
+++ rolePermission/permission.js
@@ -1,6 +1,7 @@
 import { Category } from "../model/category.model.js";
 import { CompanyDetails } from "../model/companyDetails.model.js";
 import { Customer } from "../model/customer.model.js";
+import { Promotion } from "../model/promotion.model.js";
 import { Unit } from "../model/unit.model.js";
 import { User } from "../model/user.model.js";
 import { Warehouse } from "../model/warehouse.model.js";
@@ -181,6 +182,32 @@ export const getWarehouseHierarchy = async function getCustomerHierarchy(parentI
             .populate({ path: "rolename", model: "role" })
             .populate({ path: "created_by", model: "user" });
         const customers = await Warehouse.find({ created_by: parentId }).populate({ path: "created_by", model: "user" });
+        let results = customers;
+        for (const user of users) {
+            const subResults = await getCustomerHierarchy(user._id, processedIds);
+            results = results.concat(subResults);
+        }
+        for (const customer of customers) {
+            const subResults = await getCustomerHierarchy(customer._id, processedIds);
+            results = results.concat(subResults);
+        }
+        return results;
+    } catch (error) {
+        console.error('Error in getCustomerHierarchy:', error);
+        throw error;
+    }
+};
+
+export const getPromotionHierarchy = async function getCustomerHierarchy(parentId, processedIds = new Set()) {
+    try {
+        if (processedIds.has(parentId)) {
+            return [];
+        }
+        processedIds.add(parentId);
+        const users = await User.find({ created_by: parentId, status: 'Active' })
+            .populate({ path: "rolename", model: "role" })
+            .populate({ path: "created_by", model: "user" });
+        const customers = await Promotion.find({ created_by: parentId }).populate({ path: "created_by", model: "user" });
         let results = customers;
         for (const user of users) {
             const subResults = await getCustomerHierarchy(user._id, processedIds);
