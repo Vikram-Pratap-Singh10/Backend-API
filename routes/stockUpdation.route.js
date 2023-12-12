24731c6e6eefc6e55063e148811c6137dd9981bc
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2023-12-12 15:39:32 +0530

    stock-in-word

--- routes/stockUpdation.route.js
+++ routes/stockUpdation.route.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { stockTransferToWarehouse, viewWarehouse, viewWarehouseStock } from "../controller/stockUpdation.controller.js";
+import { stockTransferToWarehouse, updateWarehousetoWarehouse, viewInWardStockToWarehouse, viewOutWardStockToWarehouse, viewWarehouseStock } from "../controller/stockUpdation.controller.js";
 
 const router = express.Router();
 
 router.post("/stock-transfer-warehouse", stockTransferToWarehouse)
-router.get("/view-warehouse-stock", viewWarehouse)
+router.get("/view-in-ward-stock/:id", viewInWardStockToWarehouse)
+router.get("/view-out-ward-stock/:id", viewOutWardStockToWarehouse)
 
 router.get("/view-warehouse-stock/:id", viewWarehouseStock)
+router.put("/update-warehoue-to-warehouse/:id", updateWarehousetoWarehouse)
 
 export default router;
