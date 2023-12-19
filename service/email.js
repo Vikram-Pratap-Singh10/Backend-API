f3f97b8dd6661ee7334dd4a71dedf3362907aef1
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2023-12-19 10:07:20 +0530

    view stock

--- service/email.js
+++ service/email.js
@@ -1,11 +1,13 @@
-import { createTransport } from 'nodemailer';
+import nodemailer from "nodemailer";
 
-const transporter = createTransport({
-    host: "smtp-relay.brevo.com",
-    port: 587,
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
-        user: "noreply@softnumen.com",
-        pass: "N7f4z61E5DLZKbRS",
+        user: 'vikramsveltose022@gmail.com',
+        pass: 'hpjswgwumxuupbsa'
     },
 });
-export default transporter
+export default transporter;
